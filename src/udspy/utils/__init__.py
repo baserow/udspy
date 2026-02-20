c511d531fc09938eba768b2af0e95e8725f7fd41
Davide Silvestri <silvestri.eng@gmail.com>
2026-02-20 10:58:51 +0100

    refactor(utils): improve error formatting for LLM consumption
    - Rewrite format_tool_exception to show error message first, then frames
    - Add max_length truncation to prevent context bloat
    - Add format_validation_error with schema hint for the LLM
    - Export format_validation_error from utils package

--- src/udspy/utils/__init__.py
+++ src/udspy/utils/__init__.py
@@ -7,7 +7,7 @@ This package contains utility functions organized by category:
 """
 
 from udspy.utils.async_support import ensure_sync_context, execute_function_async
-from udspy.utils.formatting import format_tool_exception
+from udspy.utils.formatting import format_tool_exception, format_validation_error
 from udspy.utils.schema import minimize_schema, resolve_json_schema_reference
 
 __all__ = [
@@ -16,4 +16,5 @@ __all__ = [
     "resolve_json_schema_reference",
     "minimize_schema",
     "format_tool_exception",
+    "format_validation_error",
 ]
